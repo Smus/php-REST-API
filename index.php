208702b73ef3bc2b134f400237e218a882f63bf7
Rasmus Bidstrup <rb@hjaltelinstahl.com>
2018-10-14 14:41:41 +0200

    botxo

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php
+/******** GET/POST *********/
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    # code...
-} if else ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    # code...
+    http_response_code(200);
+    // code...
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $postBody = file_get_contents('php://input');
+    $postBody = json_decode($postBody);
+
+    ////// BotXO
+    //// customVariables data
+    //print_r( $postBody->customVariables );
+
+    //// extracted data
+    //print_r( $postBody->extracted );
+
+    if ($postBody) {
+        http_response_code(200);
+    } else {
+        http_response_code(500);
+    }
+    
+    //print_r($postBody);
+    WriteToLog($postBody);
+
 } else {
     http_response_code(405);
 }
+
+/******** Write to log file *********/
+function WriteToLog($txt){
+    $txt = str_replace('\"',"\n",json_encode($txt));
+    $myfile = file_put_contents('log/logs.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+}
+/*********************************/
+
 ?>
